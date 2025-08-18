25e5119aca3c32dc35af6d1a70869ee42f4d9269
Viacheslav <viacheslav_khylchuk@ukr.net>
2025-08-18 19:19:48 +0200

    version2

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -65,25 +65,21 @@ const images = [
 		description: "Lighthouse Coast Sea",
 	},
 ]
-const galleryBlock = document.querySelector(".gallery")
-for (const image of images) {
-	const { preview, original, description } = image
-	galleryBlock.insertAdjacentHTML(
-		"beforeend",
-		`
-	<li class="gallery-item">
-		<a class="gallery-link" href="${original}">
-			<img
-				class="gallery-image"
-				src="${preview}"
-				data-source="${original}"
-				alt="${description}"
-			/>
-		</a>
-	</li>`
-	)
-}
+const galleryBlock = document.querySelector(".gallery");
+const galleryMarkup = images.map(({ preview, original, description }) => `
+  <li class="gallery-item">
+    <a class="gallery-link" href="${original}">
+      <img
+        class="gallery-image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`).join("");
+galleryBlock.insertAdjacentHTML("beforeend", galleryMarkup);
 const gallery = new SimpleLightbox(".gallery a", {
-	captionsData: "alt",
-	captionDelay: 250,
-})
+  captionsData: "alt",
+  captionDelay: 250,
+});
